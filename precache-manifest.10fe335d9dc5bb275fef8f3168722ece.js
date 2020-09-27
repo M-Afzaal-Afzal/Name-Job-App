3ab2ace2979bce99fe9a84fdd3eeb48fd9176a42
M Afzaal Afzal <afzaal1721@gmail.com>
2020-09-27 11:48:22 +0500

    Build Everything
    Basic add or remove persons app to learn basics of react

---
+++ precache-manifest.10fe335d9dc5bb275fef8f3168722ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59818bd8b66b2cba27699978616d41ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5239158faa5bbbc14cca",
+    "url": "/static/css/main.63f65102.chunk.css"
+  },
+  {
+    "revision": "efa06d51dba04e56b0fd",
+    "url": "/static/js/2.7d7746bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d7746bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5239158faa5bbbc14cca",
+    "url": "/static/js/main.96dc60a7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "660b9d1560b1b414900d9bfe6b707c21",
+    "url": "/static/media/background-min.660b9d15.webp"
+  }
+]);
